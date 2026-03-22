66067fe0fb35ed1916a64d1ca6493eea5cee6767
RussellLuo <luopeng.he@gmail.com>
2026-03-23 00:03:44 +0800

    fix(lint): align VoiceConfig env tags

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -565,7 +565,7 @@ type DevicesConfig struct {
 
 type VoiceConfig struct {
 	ModelName         string `json:"model_name,omitempty" env:"PICOCLAW_VOICE_MODEL_NAME"`
-	EchoTranscription bool   `json:"echo_transcription" env:"PICOCLAW_VOICE_ECHO_TRANSCRIPTION"`
+	EchoTranscription bool   `json:"echo_transcription"   env:"PICOCLAW_VOICE_ECHO_TRANSCRIPTION"`
 }
 
 type ProvidersConfig struct {
